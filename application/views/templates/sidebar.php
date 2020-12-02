a36fae7b931b2c9fdfd0063e3516a70b392962ab
miarsmt <rusmiatim973@gmail.com>
2020-12-02 10:50:48 +0700

    ubah title

--- application/views/templates/sidebar.php
+++ application/views/templates/sidebar.php
@@ -6,7 +6,7 @@
         <div class="sidebar-brand-icon rotate-n-15">
             <i class="fas fa-code"></i>
         </div>
-        <div class="sidebar-brand-text mx-3">CI Admin </div>
+        <div class="sidebar-brand-text mx-3">SIMAG </div>
     </a>
 
     <!-- Divider -->
