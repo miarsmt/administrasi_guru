128b143a2c3ecaaf1dd033718c0ad8ba98528ff6
miarsmt <rusmiatim973@gmail.com>
2020-12-02 10:50:12 +0700

    hapus card header

--- application/views/user/change_password.php
+++ application/views/user/change_password.php
@@ -9,9 +9,6 @@
     <div class="row">
         <div class="col-lg-6">
             <div class="card shadow mb-4">
-                <div class="card-header py-3">
-                    <h6 class="m-0 font-weight-bold text-primary"></h6>
-                </div>
                 <div class="card-body">
                     <?= $this->session->flashdata('message'); ?>
                     <form action="<?= base_url('user/changepassword'); ?>" method="post">
