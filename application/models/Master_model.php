1db04cc8b008076026f57490e90d5fbe1de2c74f
miarsmt <rusmiatim973@gmail.com>
2020-12-14 10:03:08 +0700

    menambahkan modul mata pelajaran

--- application/models/Master_model.php
+++ application/models/Master_model.php
@@ -193,4 +193,66 @@ class Master_model extends CI_Model
         $this->db->delete('tb_siswa', ['nis' => $id]);
         return true;
     }
+
+    public function getMapel()
+    {
+        $this->db->select('tb_mapel.*, tb_jurusan.namajurusan');
+        $this->db->from('tb_mapel');
+        $this->db->join('tb_jurusan', 'tb_jurusan.kodejurusan = tb_mapel.kodejurusan', 'left');
+        $result = $this->db->get();
+        return $result->result_array();
+    }
+
+    public function kode_mp()
+    {
+        $this->db->select('RIGHT(tb_mapel.kodemapel, 2) as kode', FALSE);
+        $this->db->from('tb_mapel');
+        $this->db->order_by('kodemapel', 'DESC');
+        $this->db->limit(1);
+        $query = $this->db->get('');      //cek dulu apakah ada sudah ada kode di tabel.    
+        if ($query->num_rows() <> 0) {
+            //jika kode ternyata sudah ada.      
+            $data = $query->row();
+            $kode = intval($data->kode) + 1;
+        } else {
+            //jika kode belum ada      
+            $kode = 1;
+        }
+        $kodemax = str_pad($kode, 3, "0", STR_PAD_LEFT); // angka 3 menunjukkan jumlah digit angka 0
+        $kodehasil = "MP" . $kodemax;
+        return $kodehasil;
+    }
+
+    public function save_mapel($data)
+    {
+        $this->db->insert('tb_mapel', $data);
+        return true;
+    }
+
+    public function getMapelById($id)
+    {
+        return $this->db->get_where('tb_mapel', ['kodemapel' => $id])->row_array();
+    }
+
+    public function update_mapel()
+    {
+        $data = [
+            'namamapel'     => $this->input->post('namamp', true),
+            'tingkatan'     => $this->input->post('tingkatan', true),
+            'kelompok'      => $this->input->post('kelompok', true),
+            'kodejurusan'   => $this->input->post('kodejur', true),
+            'kkm'           => $this->input->post('kkm', true),
+            'iduser'        => $this->session->userdata('role_id')
+        ];
+
+        $this->db->where('kodemapel', $this->input->post('kode'));
+        $this->db->update('tb_mapel', $data);
+        return true;
+    }
+
+    public function delmapel($id)
+    {
+        $this->db->delete('tb_mapel', ['kodemapel' => $id]);
+        return true;
+    }
 }
---
+++ application/views/master/mapel/index.php
@@ -0,0 +1,123 @@
+<!-- Begin Page Content -->
+<div class="container-fluid">
+
+    <!-- Page Heading -->
+    <h1 class="h3 mb-4 text-gray-800"><?= $title; ?></h1>
+
+    <div class="row">
+        <div class="col-lg">
+
+            <!-- kalau error -->
+            <?php if (validation_errors()) : ?>
+                <div class="alert alert-danger alert-dismissible">
+                    <button type="button" class="close" data-dismiss="alert" aria-hidden="true">x</button>
+                    <?= validation_errors(); ?>
+                </div>
+            <?php endif; ?>
+
+            <!-- kalau lolos -->
+            <div class="flash-data" data-flashdata="<?= $this->session->flashdata('message'); ?>"></div>
+
+            <div class="card shadow mb-4">
+                <div class="card-header py-3">
+                    <a href="" data-toggle="modal" data-target="#newMapelModal" class="btn btn-info btn-sm float-right bg-gradient-info"><i class="fas fa-plus-circle"></i> Tambah Mata Pelajaran</a>
+                </div>
+                <div class="card-body">
+                    <div class="table-responsive">
+                        <table class="table table-hover table-bordered" id="dataTable" width="100%" cellspacing="0">
+                            <thead>
+                                <tr>
+                                    <th scope="col">#</th>
+                                    <th scope="col">Kode Mapel</th>
+                                    <th scope="col">Nama Mapel</th>
+                                    <th scope="col">KKM</th>
+                                    <th scope="col">Tingkatan</th>
+                                    <th scope="col">Kelompok</th>
+                                    <th scope="col">Jurusan</th>
+                                    <th scope="col">Action</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <?php $i = 1; ?>
+                                <?php foreach ($mapel as $mp) : ?>
+                                    <?php if ($mp['kodejurusan'] == 'Semua Jurusan') {
+                                        $jurusan = $mp['kodejurusan'];
+                                    } else {
+                                        $jurusan = $mp['namajurusan'];
+                                    } ?>
+                                    <tr>
+                                        <th scope="row"><?= $i; ?></th>
+                                        <td><?= $mp['kodemapel']; ?></td>
+                                        <td><?= $mp['namamapel']; ?></td>
+                                        <td><?= $mp['kkm']; ?></td>
+                                        <td><?= $mp['tingkatan']; ?></td>
+                                        <td><?= $mp['kelompok']; ?></td>
+                                        <td><?= $jurusan; ?></td>
+                                        <td>
+                                            <a href="<?= base_url(); ?>master/editmapel/<?= $mp['kodemapel']; ?>" class="btn btn-warning btn-circle btn-sm bg-gradient-warning" title="Edit Data"><i class="fas fa-edit"></i></a>
+                                            <a href="<?= base_url(); ?>master/deletemapel/<?= $mp['kodemapel']; ?>" class="btn btn-danger btn-circle btn-sm bg-gradient-danger tombol-hapus" title="Hapus Data"><i class="fas fa-trash"></i></a>
+                                        </td>
+                                    </tr>
+                                    <?php $i++; ?>
+                                <?php endforeach; ?>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+</div>
+<!-- /.container-fluid -->
+
+</div>
+<!-- End of Main Content -->
+
+<!-- Modal Add -->
+
+<div class="modal fade" id="newMapelModal" tabindex="-1" aria-labelledby="newMapelModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="newMapelModalLabel">Tambah Mata Pelajaran</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <form action="<?= base_url('master/mapel'); ?>" method="post">
+                <div class="modal-body">
+                    <div class="form-group">
+                        <input type="text" class="form-control" name="kodemp" id="kodemp" value="<?= $kodemp; ?>">
+                    </div>
+                    <div class="form-group">
+                        <input type="text" class="form-control" name="namamp" id="namamp" placeholder="Nama Mapel">
+                    </div>
+                    <div class="form-group">
+                        <input type="text" class="form-control" name="tingkatan" id="tingkatan" placeholder="Tingkatan">
+                    </div>
+                    <div class="form-group">
+                        <input type="text" class="form-control" name="kelompok" id="kelompok" placeholder="Kelompok Mapel">
+                    </div>
+                    <div class="form-group">
+                        <select name="kodejur" id="kodejur" class="form-control">
+                            <option value="">-- Jurusan --</option>
+                            <option value="Semua Jurusan">Semua Jurusan</option>
+                            <?php foreach ($jrsn as $jr) : ?>
+                                <option value="<?= $jr['kodejurusan']; ?>"><?= $jr['namajurusan']; ?></option>
+                            <?php endforeach; ?>
+                        </select>
+                    </div>
+                    <div class="form-group">
+                        <input type="text" class="form-control" name="kkm" id="kkm" placeholder="KKM">
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                    <button type="submit" class="btn btn-primary">Simpan</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
