b2b5653833d7fbfc193f8b13adf23729df976fe2
miarsmt <rusmiatim973@gmail.com>
2020-12-14 22:29:48 +0700

    mengubah brand

--- application/views/templates/sidebar.php
+++ application/views/templates/sidebar.php
@@ -6,7 +6,7 @@
         <div class="sidebar-brand-icon rotate-n-15">
             <i class="fas fa-code"></i>
         </div>
-        <div class="sidebar-brand-text mx-3">SIMAG </div>
+        <div class="sidebar-brand-text mx-3">SMK PASIM Plus</div>
     </a>
 
     <!-- Divider -->
