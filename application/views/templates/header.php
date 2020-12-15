5e04c1ccbca441d83e7ed777de3a16b13b2e8ddf
miarsmt <rusmiatim973@gmail.com>
2020-12-15 13:38:43 +0700

    modifikasi icon shortcut

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -20,6 +20,7 @@
 
     <!-- Custom styles for this page -->
     <link href="<?= base_url('assets/'); ?>vendor/datatables/dataTables.bootstrap4.min.css" rel="stylesheet">
+    <link rel="shortcut icon" href="<?= base_url('assets/img/logo.png'); ?>">
 
 </head>
 
--- application/views/templates/sidebar.php
+++ application/views/templates/sidebar.php
@@ -4,7 +4,7 @@
     <!-- Sidebar - Brand -->
     <a class="sidebar-brand d-flex align-items-center justify-content-center" href="javascript:;">
         <div class="sidebar-brand-icon rotate-n-15">
-            <i class="fas fa-code"></i>
+            <img src="<?= base_url('assets/img/logo.png'); ?>" width="60px">
         </div>
         <div class="sidebar-brand-text mx-3">SMK PASIM Plus</div>
     </a>
