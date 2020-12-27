962bba51f6a1a36cd1e2bfb931ffaeb292526485
miarsmt <rusmiatim973@gmail.com>
2020-12-27 22:08:39 +0700

    menambahkan fitur import data mapel

--- application/controllers/Import.php
+++ application/controllers/Import.php
@@ -36,4 +36,36 @@ class Import extends CI_Controller
             redirect('master/guru');
         }
     }
+
+    public function uploadmapel()
+    {
+        $file_mimes = ['application/octet-stream', 'application/vnd.ms-excel', 'application/x-csv', 'text/x-csv', 'text/csv', 'application/csv', 'application/excel', 'application/vnd.msexcel', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'];
+
+        if (isset($_FILES['berkas_excel']['name']) && in_array($_FILES['berkas_excel']['type'], $file_mimes)) {
+            $arr_file = explode('.', $_FILES['berkas_excel']['name']);
+            $extension = end($arr_file);
+
+            if ('csv' == $extension) {
+                $reader = new \PhpOffice\PhpSpreadsheet\Reader\Csv();
+            } else {
+                $reader = new \PhpOffice\PhpSpreadsheet\Reader\Xlsx();
+            }
+
+            $spreadsheet = $reader->load($_FILES['berkas_excel']['tmp_name']);
+
+            $sheetData = $spreadsheet->getActiveSheet()->toArray();
+            for ($i = 1; $i < count($sheetData); $i++) {
+                $data = [
+                    'kodemapel' => $sheetData[$i]['1'],
+                    'namamapel' => $sheetData[$i]['2'],
+                    'tingkatan' => $sheetData[$i]['3'],
+                    'kodejurusan' => $sheetData[$i]['5']
+                ];
+
+                $this->db->insert('tb_mapel', $data);
+            }
+            $this->session->set_flashdata('message', 'data mapel berhasil di-import');
+            redirect('master/mapel');
+        }
+    }
 }
