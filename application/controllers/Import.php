def452b9ea0c12fd115cfae115d6ea0718911e2e
miarsmt <rusmiatim973@gmail.com>
2020-12-27 23:11:33 +0700

    menambahkan fitur import data siswa

--- application/controllers/Import.php
+++ application/controllers/Import.php
@@ -68,4 +68,38 @@ class Import extends CI_Controller
             redirect('master/mapel');
         }
     }
+
+    public function uploadsiswa()
+    {
+        $file_mimes = ['application/octet-stream', 'application/vnd.ms-excel', 'application/x-csv', 'text/x-csv', 'text/csv', 'application/csv', 'application/excel', 'application/vnd.msexcel', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'];
+
+        if (isset($_FILES['berkas_excel']['name']) && in_array($_FILES['berkas_excel']['type'], $file_mimes)) {
+            $arr_file = explode('.', $_FILES['berkas_excel']['name']);
+            $extension = end($arr_file);
+
+            if ('csv' == $extension) {
+                $reader = new \PhpOffice\PhpSpreadsheet\Reader\Csv();
+            } else {
+                $reader = new \PhpOffice\PhpSpreadsheet\Reader\Xlsx();
+            }
+
+            $spreadsheet = $reader->load($_FILES['berkas_excel']['tmp_name']);
+
+            $sheetData = $spreadsheet->getActiveSheet()->toArray();
+            for ($i = 1; $i < count($sheetData); $i++) {
+                $data = [
+                    'nis'           => $sheetData[$i]['1'],
+                    'namasiswa'     => $sheetData[$i]['2'],
+                    'jeniskelamin'  => $sheetData[$i]['4'],
+                    'kodekelas'     => $sheetData[$i]['14'],
+                    'kodejurusan'   => $sheetData[$i]['15'],
+                    'semester_aktif' => $sheetData[$i]['16']
+                ];
+
+                $this->db->insert('tb_siswa', $data);
+            }
+            $this->session->set_flashdata('message', 'data siswa berhasil di-import');
+            redirect('master/siswa');
+        }
+    }
 }
