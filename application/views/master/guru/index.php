15c0281ff038cd9aadfb870a2cf71ad436cd969c
miarsmt <rusmiatim973@gmail.com>
2020-12-28 12:23:09 +0700

    mengubah action form import file excel

--- application/views/master/guru/index.php
+++ application/views/master/guru/index.php
@@ -77,7 +77,7 @@
                     <span aria-hidden="true">&times;</span>
                 </button>
             </div>
-            <form action="<?= base_url('import/uploadguru'); ?>" method="post" enctype="multipart/form-data">
+            <form action="<?= base_url('import/upload'); ?>" method="post" enctype="multipart/form-data">
                 <div class="modal-body">
                     <div class="form-group">
                         <label for="file">Unggah File Excel</label>
