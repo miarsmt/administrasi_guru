ae88334b3d2d5facc3a6f3c9724b79e54965c763
miarsmt <rusmiatim973@gmail.com>
2020-12-28 21:54:50 +0700

    mengubah function addkomp untuk dipidahkan di class lain

--- application/controllers/Master.php
+++ application/controllers/Master.php
@@ -583,52 +583,6 @@ class Master extends CI_Controller
         redirect('master/mapel');
     }
 
-    public function addkomp($id)
-    {
-        $data = [
-            'title'     => 'Add Kompetensi Dasar',
-            'user'      => $this->admin->sesi(),
-            'kompdasar' => $this->master->getKompdasar($id),
-            'mapel'     => $this->master->getMapelById($id)
-        ];
-
-        $dmapel    = $this->master->getMapelById($id);
-
-        $this->form_validation->set_rules('kodekd', 'Kode KD', 'required|trim', [
-            'required' => '%s tidak boleh kosong'
-        ]);
-        $this->form_validation->set_rules('namakd', 'Nama KD', 'required|trim', [
-            'required' => '%s tidak boleh kosong'
-        ]);
-        $this->form_validation->set_rules('semester', 'Semester', 'required|trim', [
-            'required' => '%s tidak boleh kosong'
-        ]);
-        $this->form_validation->set_rules('kkm', 'KKM', 'required|trim', [
-            'required' => '%s tidak boleh kosong'
-        ]);
-
-        if ($this->form_validation->run() == false) {
-            $this->load->view('templates/header', $data);
-            $this->load->view('templates/sidebar', $data);
-            $this->load->view('templates/topbar', $data);
-            $this->load->view('master/mapel/add_komp', $data);
-            $this->load->view('templates/footer');
-        } else {
-            $data = [
-                'kodekd'    => $this->input->post('kodekd', true),
-                'namakd'    => $this->input->post('namakd', true),
-                'semester'  => $this->input->post('semester', true),
-                'kkm'       => $this->input->post('kkm', true),
-                'kodemapel' => $id,
-                'iduser'    => $this->session->userdata('role_id')
-            ];
-
-            $this->master->save_komp($data);
-            $this->session->set_flashdata('message', 'data kompetensi dasar ' . $dmapel['namamapel'] . ' berhasil ditambah-kan');
-            redirect('master/mapel');
-        }
-    }
-
     public function mengajar()
     {
         $data = [
