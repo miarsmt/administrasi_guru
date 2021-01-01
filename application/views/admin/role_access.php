952e67728b38f7484addf9bff724f1154fb4da36
miarsmt <rusmiatim973@gmail.com>
2021-01-01 08:04:11 +0700

    merubah ukuran grid card

--- application/views/admin/role_access.php
+++ application/views/admin/role_access.php
@@ -5,7 +5,7 @@
     <h1 class="h3 mb-4 text-gray-800"><?= $title; ?></h1>
 
     <div class="row">
-        <div class="col-lg-6">
+        <div class="col-lg-7">
 
             <!-- kalau lolos -->
             <div class="flash-data" data-flashdata="<?= $this->session->flashdata('message'); ?>"></div>
