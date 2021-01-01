abf8d9fef9b4d8f984522896d393c8dde1e5d260
miarsmt <rusmiatim973@gmail.com>
2021-01-01 08:03:21 +0700

    merubah ukuran grid card

--- application/views/admin/role.php
+++ application/views/admin/role.php
@@ -5,7 +5,7 @@
     <h1 class="h3 mb-4 text-gray-800"><?= $title; ?></h1>
 
     <div class="row">
-        <div class="col-lg-6">
+        <div class="col-lg-7">
 
             <!-- kalau error -->
             <?= form_error('role', '<div class="alert alert-danger" role="alert">', '</div>') ?>
