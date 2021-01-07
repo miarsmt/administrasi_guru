3c8427a7f18cc0696cabb40bafb01f1f55467e7c
miarsmt <rusmiatim973@gmail.com>
2021-01-07 14:07:06 +0700

    rubah content mapel diampu

--- application/views/guru/ampu.php
+++ application/views/guru/ampu.php
@@ -8,47 +8,39 @@
 
     <div class="row">
         <div class="col-lg-12">
-            <div class="card mb-4 py-3 border-left-danger">
-                <div class="card-body">
-                    <dt>Cek terlebih dahulu kompetensi dasar masing-masing mata pelajaran</dt>
-                    <dd>Apabila ada mata pelajaran yang belum di-input-kan kompetensi dasar nya, silahkan tambahkan terlebih dahulu supaya memudahkan dalam pengisian agenda kegiatan</dd>
-                </div>
+            <div class="alert alert-warning bg-warning" style="color: #000">
+                <b>Petunjuk : </b><br>
+                Menu ini digunakan untuk menginput nilai pada setiap masing-masing mata pelajaran diampu. Silakan klik menu <b><i>Nilai Pengetahuan</i></b> untuk menginput nilai pengetahuan, dan <b><i>Nilai Keterampilan</i></b> untuk menginput nilai keterampilan.
             </div>
         </div>
-        <div class="col-lg-12">
-            <div class="card shadow mb-4 py-3">
-                <div class="card-body">
-                    <table class="table table-striped">
-                        <thead>
-                            <tr>
-                                <th scope="col">#</th>
-                                <th scope="col">Mata Pelajaran</th>
-                                <th scope="col">Semester</th>
-                                <th scope="col">Kelas</th>
-                                <th scope="col">Tahun Ajaran</th>
-                                <th scope="col">&nbsp;</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <?php $i = 1; ?>
-                            <?php foreach ($ampu as $am) : ?>
-                                <tr>
-                                    <th scope="row"><?= $i; ?></th>
-                                    <td><?= $am['namamapel']; ?></td>
-                                    <td><?= $am['semester']; ?></td>
-                                    <td><?= $am['kelas']; ?> <?= $am['namakelas']; ?></td>
-                                    <td><?= $am['periode_mengajar']; ?></td>
-                                    <td>
-                                        <a href="<?= base_url('guru/addkomp/' . $am['kodemapel']); ?>" class="badge badge-info">Cek KD</a>
-                                    </td>
-                                </tr>
-                                <?php $i++; ?>
-                            <?php endforeach; ?>
-                        </tbody>
-                    </table>
+
+        <?php
+        if (!empty($ampu)) {
+            foreach ($ampu as $a) {
+        ?>
+                <div class="col-xl-4 col-md-6 mb-4">
+                    <div class="card shadow">
+                        <div class="card-body">
+                            <div class="row no-gutters align-items-center">
+                                <div class="col mr-2">
+                                    <div class="text-xs font-weight-bold text-primary text-uppercase mb-1"><?= $a['namamapel']; ?></div>
+                                    <div class="h5 mb-0 font-weight-bold text-gray-800"><?= $a['kelas']; ?> <?= $a['namakelas']; ?></div>
+                                </div>
+                            </div>
+                            <ul class="list-group mt-2">
+                                <li class="list-group-item"><a href="<?= base_url('guru/n_pengetahuan/' . $a['idmengajar']); ?>"><i class="fas fa-fw fa-chevron-right"></i> Nilai Pengetahuan</a></li>
+                                <li class="list-group-item"><a href="<?= base_url('guru/n_keterampilan/' . $a['idmengajar']); ?>"><i class="fas fa-fw fa-chevron-right"></i> Nilai Keterampilan</a></li>
+                            </ul>
+                        </div>
+                    </div>
                 </div>
-            </div>
-        </div>
+        <?php
+            }
+        } else {
+            echo '<div class="alert alert-info">Belum ada mapel yang diampu..</div>';
+        }
+        ?>
+
     </div>
 
 </div>
